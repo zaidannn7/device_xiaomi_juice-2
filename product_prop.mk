dveofficial/device_xiaomi_juice

juice: Add props to revert latching changes
Signed-off-by: aseelps <aseelpsathar@gmail.com>

product_prop.mk
@@ -12,7 +12,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    debug.sf.latch_unsignaled=0
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \

zaidannn7/device_xiaomi_juice-2

product_prop.mk
@@ -20,7 +20,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiagl \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    debug.sf.latch_unsignaled=0
+    debug.sf.latch_unsignaled=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \