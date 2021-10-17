3c01247300751c28735c5740a82fb1fe4e5ff9e4
TyraLizQ10 <nathanielquitua@gmail.com>
2021-10-17 10:24:23 +0800

    juice: Update Redfin fingerprint to October

--- corvus_juice.mk
+++ corvus_juice.mk
@@ -22,8 +22,16 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
-
 # Corvus Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.corvus.maintainer=Nathaniel_Quitua
+	
+# Fingerprint
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.211001.001.A1 7641976 release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
