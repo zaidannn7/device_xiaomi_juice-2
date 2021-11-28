95b4e628e7f394883cc1ef3a7c6c1f6e925ba282
elizabethangelalorenza <elizabethangelalorenzha@gmail.com>
2021-11-28 13:22:00 +0000

    juice: Update RemovePackages
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,19 +4,53 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService arcore
-LOCAL_OVERRIDES_PACKAGES += CarrierSetup Chrome Chrome-Stub ConnMO ConnMetrics
-LOCAL_OVERRIDES_PACKAGES += DCMO DevicePolicyPrebuilt DMService Drive 
-LOCAL_OVERRIDES_PACKAGES += GCS
-LOCAL_OVERRIDES_PACKAGES += MaestroPrebuilt Maps MyVerizonServices NovaBugReportWrapper Ornament OemDmTrigger OBDM_Permissions
-LOCAL_OVERRIDES_PACKAGES += PixelLiveWallpaperPrebuilt PrebuiltGmail FilesPrebuilt
-LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SCONE ScribePrebuilt Showcase SoundAmplifierPrebuilt SprintDM SprintHM
-LOCAL_OVERRIDES_PACKAGES += Tycho
-LOCAL_OVERRIDES_PACKAGES += USCCDM Photos
-LOCAL_OVERRIDES_PACKAGES += Videos VZWAPNLib VzwOmaTrigger obdm_stub
-LOCAL_OVERRIDES_PACKAGES += WallpapersBReel2020
-LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt GoogleCamera CalculatorGooglePrebuilt Snap Camera2
+LOCAL_OVERRIDES_PACKAGES := \
+    AmbientSensePrebuilt \
+    arcore \
+    Browser \
+    Camera2 \
+    Chrome \
+    Chrome-Stub \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DMService \
+    Drive \
+    GCS \
+    GoogleCamera \
+    Maps \
+    Music \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PixelLiveWallpaperPrebuilt \
+    Photos \
+    PlayAutoInstallConfig \
+    PrebuiltGmail \
+    RecorderPrebuilt \
+    SafetyHubPrebuilt \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    Snap2 \
+    SnapdragonCamera \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    Tycho \
+    USCCDM \
+    Videos \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    YouTube \
+    YouTubeMusicPrebuilt
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
