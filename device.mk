69436b9a9e6c466718184334067b96df6b87b36e
Muhammad Fadly A S <mhmmdfdlyas@gmail.com>
2021-12-12 07:47:21 +0700

    juice: rro_overlays: Remove Gms overlay
    Change-Id: I6b8721b316baeeae9d213d89824ca27fed9dded1
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -148,8 +148,6 @@ PRODUCT_PACKAGES += \
     CellBroadcastReceiverResCommon \
     FrameworksResCommon \
     FrameworksResTarget \
-    GmsTelecommOverlay \
-    GmsTelephonyOverlay \
     NotchBarKiller \
     SystemUIResCommon \
     TelecommResCommon \
