e5931b101132267809064c171ab441e16fdc51eb
zaidannn7 <zaidanmovic0512@gmail.com>
2022-10-05 04:30:59 +0700

    juice: add TurboAdapter to remove_packages
    *and nogapss build

--- lighthouse_juice.mk
+++ lighthouse_juice.mk
@@ -8,12 +8,11 @@
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common potato stuff.
-$(call inherit-product, vendor/lighthouse/config/common_full_phone.mk)
+$(call inherit-product, vendor/lighthouse/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Gapps
-TARGET_GAPPS_ARCH := arm64
+TARGET_DEVICE=juice
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lighthouse_juice
